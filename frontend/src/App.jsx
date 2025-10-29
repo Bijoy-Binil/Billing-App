dfc3f7eded2590b5b49211be18086b54c2052552
Bijoy <bijocode@gmail.com>
2025-10-30 00:48:09 +0530

    frontend initialized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-primary'>
+
+      <h1 className='text-2xl bg '>Hii</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        primary: "#00C853",
+        accent: "#1976D2",
+        dark: "#111827",
+      },
+    },
+  },
+  plugins: [],
+};
